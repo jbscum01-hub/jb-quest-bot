767f8bd84f2dc87ea86db684aaa3b31d5dd24508
jbscum01-hub <jb.scum01@gmail.com>
2026-03-08 16:36:07 +0700

    Delete src directory

--- src/utils/safeReply.js
+++
@@ -1,8 +0,0 @@
-async function safeReply(interaction, content) {
-  if (interaction.replied || interaction.deferred) {
-    return interaction.followUp({ content, ephemeral: true });
-  }
-  return interaction.reply({ content, ephemeral: true });
-}
-
-module.exports = { safeReply };
